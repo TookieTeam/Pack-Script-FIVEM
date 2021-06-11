853da2f5da6cf2550039676cdc73b24bd10bb6d8
Tookie <82297355+SkyxTeam@users.noreply.github.com>
2021-06-11 10:06:20 +0200

    Add files via upload

---
+++ tookie_garage_ems/config.lua
@@ -0,0 +1,19 @@
+Config = {
+    
+    position = vector3(294.50, -613.70, 43.42),
+    jobname = 'ambulance',
+
+    voiture = "ems",
+    label = "Véhicule EMS",
+    ---
+    voiture2 = "dodgeEMS",
+    label2 = "Véhicule EMS Rapide",
+    ---
+    voiture3 = "ems1",
+    label3 = "Véhicule EMS SUV",
+    ---
+    voiture4 = "ambulance",
+    label4 = "Véhicule EMS lent",
+    ---
+
+}
